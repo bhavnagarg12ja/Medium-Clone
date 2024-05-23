725befaf32266091ad7ef36ba6e178f4a82faf0c
bhavnagarg12ja <bhavnagarg12ja@gmail.com>
2024-05-23 08:31:26 +0530

    labelled input

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,4 +1,5 @@
 import { Link, useNavigate } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
 
 export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     return (
@@ -22,4 +23,29 @@ export const Auth = ({ type }: { type: "signup" | "signin" }) => {
 
         </div>
     );
+};
+
+interface LabelledInputType {
+    label : string;
+    placeholder: string;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+}
+
+function LabelledInput({ label, placeholder, onChange, type }: LabelledInputType) {
+    return (
+        <div>
+            <label className="block mb-2 text-sm font-bold text-black pt-4 font-serif">
+                {label}
+            </label>
+            <input
+                onChange={onChange}
+                type={type || "text"}
+                id="first_name"
+                className="font-serif bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                placeholder={placeholder}
+                required
+            />
+        </div>
+    )
 }
